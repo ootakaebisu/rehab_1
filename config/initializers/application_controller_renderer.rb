ba01f552ee8847065401da15fa7db5460b05507b
ootakaebisu <[ootakaebisu@gmail.com]>
2021-03-29 14:48:07 +0000

    [add]画像投稿機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a201ef0d8c0a106cd38e5ed8d428eab413522993cc52a34aaabfe97d69c52635e3a20cd53561d47ad3507033086b71d0a4c38e39dd332eced3b087afb3a35413'
